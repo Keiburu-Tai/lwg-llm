f7c183a977f03fd3143af9882fc9c897a2ae4fd4
minjunj <minjun_jo@gm.gist.ac.kr>
2024-05-16 02:56:29 +0900

    first commit

--- main.py
+++ main.py
@@ -22,13 +22,8 @@ print("Done.")
 app = FastAPI()
 router = APIRouter()
 
-# Define json Obj
-class Sequences(BaseModel):
-    pre_prompt: str = None
-    do_sample: bool = True
-    top_k: int = 50
-    top_p: float = 0.7
-    max_new_tokens: int = 100
+class Content(BaseModel):
+    prompt: str = None
 
 # Use Instead Of Real DataBase
 sequence_settings = {}
@@ -37,33 +32,23 @@ sequence_settings = {}
 def read_root():
     return {"message": "Pong"}
 
-# LLM Envrionment Setting Path
-@app.post("/setting/")
-async def update_setting(sequences: Sequences):
-    sequence_settings['current'] = sequences.dict()
-    return sequence_settings['current']
-
 # LLM Question Path
-@app.post("/question/")
-async def generate_response(prompt: str):
-    if 'current' not in sequence_settings:
-        return {"error": "Settings not configured, please set them first."}
+@app.post("/question")
+async def generate_response(prompt: Content):
 
-    current_settings = sequence_settings['current']
-
-    formatted_prompt = f"Human: answering follow comment {current_settings['pre_prompt']} question is that {prompt} \n Assistant: "
+    formatted_prompt = f"Human: answering follow comment question is that {prompt} \n Assistant: "
 
     # Error Handling
     try:
         # Load LLM Setting Values
         result = pipeline(
             formatted_prompt,
-            do_sample=current_settings['do_sample'], # 확률적 샘플링 시 고려되는 상위 N개의 후보
-            top_k=current_settings['top_k'],  # 확률적 샘플링 시 고려되는 상위 N%의 후보
-            top_p=current_settings['top_p'], # 답변 횟수
+            do_sample=True, # 확률적 샘플링 시 고려되는 상위 N개의 후보
+            top_k=50,  # 확률적 샘플링 시 고려되는 상위 N%의 후보
+            top_p=0.7, # 답변 횟수
             num_return_sequences=1, # 반복 토큰 방지. 1이상이면 대게 많이 줄어듦
             repetition_penalty=2.0, # 한 번에 생성할 최대 토큰 수
-            max_new_tokens=current_settings['max_new_tokens'],
+            max_new_tokens=100,
         )
         if result and len(result) > 0:
             # Parse The Result
